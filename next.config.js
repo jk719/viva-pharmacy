eca0d0b2d22f0e8bef65b60706824e023bff5475
Jamil Kabir <jamilkabir719@gmail.com>
2024-11-08 14:15:58 -0500

    setting up with gh pages2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enable static export
-    trailingSlash: true, // Add trailing slashes to links for GitHub Pages compatibility
+    trailingSlash: true,
     images: {
-      unoptimized: true // Disable image optimization for GitHub Pages
+        unoptimized: true, // Disable image optimization for easier deployment
     },
-    basePath: 'viva-pharmacy', // Replace 'your-repo-name' with your GitHub repo name
-  };
-  
-  module.exports = nextConfig;
-  
+    basePath: process.env.NODE_ENV === 'production' ? '/viva-pharmacy' : '',
+};
+
+module.exports = nextConfig;
