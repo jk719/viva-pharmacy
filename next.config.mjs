c773f785b4aaba84a2f06d02195b2b2b389d236a
Jamil Kabir <jamilkabir719@gmail.com>
2024-11-08 13:55:58 -0500

    Prepare for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export', // Enable static export
+    trailingSlash: true, // Add trailing slashes to URLs
+    images: {
+        unoptimized: true, // Disable image optimization for GitHub Pages
+    },
+    basePath: '/your-repo-name', // Replace 'your-repo-name' with the name of your GitHub repo
+
     webpack: (config) => {
         // Disable Webpack file system cache to avoid caching issues
         config.cache = false;
