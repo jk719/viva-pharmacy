fb78fdc2555def0f0f07d0b48f05ee3df9eff24b
Jamil Kabir <jamilkabir719@gmail.com>
2024-11-11 08:57:21 -0500

    feat: add autocomplete to search bar and styling fixes for dropdown visibility

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,27 @@ import Image from "next/image";
 import Link from "next/link";
 import ClientCartIcon from "./ClientCartIcon";
 import AuthButtons from "./AuthButtons";
+import { useState } from "react";
+import products from "../data/products"; // Assuming products data is imported here
 
 export default function Navbar() {
+  const [query, setQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const handleInputChange = (e) => {
+    const input = e.target.value;
+    setQuery(input);
+
+    if (input) {
+      const results = products.filter((product) =>
+        product.name.toLowerCase().includes(input.toLowerCase())
+      );
+      setFilteredProducts(results);
+    } else {
+      setFilteredProducts([]);
+    }
+  };
+
   return (
     <nav className="p-4 bg-primary-color text-white">
       <div className="container mx-auto flex justify-between items-center">
@@ -21,14 +40,31 @@ export default function Navbar() {
             />
           </a>
         </Link>
+        
         {/* Search Bar */}
-        <div className="flex-grow mx-4">
+        <div className="flex-grow mx-4 relative">
           <input
             type="text"
+            value={query}
+            onChange={handleInputChange}
             placeholder="Search products..."
             className="w-full p-2 rounded-lg bg-white text-primary-color placeholder-primary-color focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          {filteredProducts.length > 0 && (
+            <ul className="absolute left-0 right-0 mt-1 autocomplete-dropdown">
+              {filteredProducts.map((product) => (
+                <li
+                  key={product.id}
+                  className="autocomplete-item"
+                  onClick={() => setQuery(product.name)}
+                >
+                  {product.name}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+        
         {/* Cart and Auth */}
         <div className="flex items-center space-x-6">
           <Link href="/cart" legacyBehavior>
