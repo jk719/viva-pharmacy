8b133f22bc22800d540eb1f57aa1f7c0d36ca1fe
Jamil Kabir <jamilkabir719@gmail.com>
2024-12-03 12:49:28 -0500

    Remove Clover integration files in favor of Stripe

--- src/lib/clover/config.js
+++
@@ -1,6 +0,0 @@
-export const CLOVER_CONFIG = {
-  apiKey: process.env.NEXT_PUBLIC_CLOVER_API_KEY,
-  merchantId: process.env.CLOVER_MERCHANT_ID,
-  environment: 'sandbox',
-  baseUrl: 'https://sandbox.dev.clover.com/v3'
-};
