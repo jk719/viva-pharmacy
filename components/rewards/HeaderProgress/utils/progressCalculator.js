a89cb6a54024de4bca86113eef73440aa0b8b254
Jamil Kabir <jamilkabir719@gmail.com>
2025-03-18 13:28:40 -0400

    chore: remove rewards system files
    - Removed all rewards components and related files
    - Cleaned up vivabucks API routes
    - Removed rewards store and utilities
    - Preserved functionality in backup/rewards-system branch

--- components/rewards/HeaderProgress/utils/progressCalculator.js
+++
@@ -1,7 +0,0 @@
-import { REWARD_CONSTANTS } from '@/lib/rewards/constants';
-
-export const calculateProgress = (points) => {
-  const pointsNeeded = REWARD_CONSTANTS.REWARD_RATE.POINTS_NEEDED;
-  const pointsInCycle = points % pointsNeeded;
-  return (pointsInCycle / pointsNeeded) * 100;
-};
